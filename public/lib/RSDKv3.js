965b56dabe0646821f2df63e1e984e7b247501c4
Jd-1206 <Jd1.ext4@gmail.com>
2024-10-26 17:07:35 -0400

    no way

--- public/lib/RSDKv3.js
+++ public/lib/RSDKv3.js
@@ -1,11 +1,16 @@
 // Defined for Emscripten.js to call
-function RSDK_Init()
-{
-    // TODO: Custom FS Path, settings
-    FS.chdir('/FileSystem/RSDKv3')
+function RSDK_Init() {
+    // TODO: Custom FS Path
+    FS.chdir('/FileSystem/RSDKv3');
 
-    // value, index
-    // index 0 - plus
-    _RSDK_Configure(0, 0);
+    const storedSettings = localStorage.getItem('settings');
+    if (storedSettings) {
+        const settings = JSON.parse(storedSettings);
+
+        // value, index
+        // index 0 - plus
+        _RSDK_Configure(settings.enablePlus, 0);
+    }
+
     _RSDK_Initialize();
 }
