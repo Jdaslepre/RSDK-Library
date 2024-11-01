158a1ca2435f4effe131c249f2efff9030b4ff34
Jd-1206 <Jd1.ext4@gmail.com>
2024-11-01 14:25:42 -0400

    :)

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export const metadata: Next.Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en' suppressHydrationWarning>
+            <head>
+                <link rel='manifest' href='./manifest.webmanifest' />
+            </head>
             <body className={`min-h-screen bg-background ${GeistSans.className}`}>
                 <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
                     <Splash />
