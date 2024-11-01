d21d95396f06bf683a191b70318ef935ab6754fb
Jd-1206 <Jd1.ext4@gmail.com>
2024-11-01 14:18:32 -0400

    just gonna try another time?

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
-
 const nextConfig = {
   output: 'export',
   reactStrictMode: false,
@@ -14,14 +9,4 @@ const nextConfig = {
   },
 };
 
-const nextConfigFunction = async (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = (await import("@ducanh2912/next-pwa")).default({
-      dest: "public",
-    });
-    return withPWA(nextConfig);
-  }
-  return nextConfig;
-};
-
-export default nextConfigFunction;
+export default nextConfig;
