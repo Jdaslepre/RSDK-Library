b03655b4f82193f6e63e2173a7e6cdde087f4e4d
Jdsle <Jd1.ext4@gmail.com>
2025-02-01 20:21:23 -0500

    been a while

--- README.md
+++ README.md
@@ -24,10 +24,15 @@ The settings page currently provides two options:
 
 ## Known Issues
 
+#### Website
+
+- Uploading files *might* hang. Give it some time before trying to reload the page
+
 #### Engines
 
 - The speed of the engines might be inaccurate to the settings.ini configuration. For now, just lower your refresh rate™️
 - The engine might freeze when hiding the toolbar on safari
+- Touch coordinates may be inaccurate if the canvas' size is not perfectly scaled
 - [RSDKv5/U Specific] The engine(s) might fail to start if you are on an older browser, mobile browser, or if the site is hosted over http instead of https 
 - [RSDKv5/U Specific] Native ModAPI probably won't work. Fine for legacy games though
 
--- app/manifest.ts
+++ app/manifest.ts
@@ -2,10 +2,10 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'RSDK-Library',
+    name: 'RSDK',
     short_name: 'RSDK',
     start_url: '.',
-    theme_color: "#231F20",
+    theme_color: "#161616",
     display: 'standalone',
     display_override: ["window-controls-overlay"],
     icons: [
@@ -15,7 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         "type": "image/png"
       },
       {
-        "src": ".icons/RSDK_Maskable.png",
+        "src": "./icons/RSDK_Maskable_256.png",
+        "sizes": "256x256",
+        "type": "image/png",
+        "purpose": "maskable"
+      },
+      {
+        "src": "./icons/RSDK_Maskable_128.png",
         "sizes": "128x128",
         "type": "image/png",
         "purpose": "maskable"
